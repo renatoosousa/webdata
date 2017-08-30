8ebcabcbc109640c7dccf933d2c78a07668e1b85
cristiano <cso@cin.ufpe.br>
2017-08-30 15:15:27 -0300

    Modification and ending this code

--- crawler/study_codes/simple_scraping.py
+++ crawler/study_codes/simple_scraping.py
@@ -10,4 +10,34 @@ def parse_options_data(table):
 	rows = table.findall('.//tr')
 	header = _unpack(rows[0], kind ='th')
 	data = [_unpack(r) for r in rows[1:]]
-	return TextParser()
+	return TextParser(data, names=header).get_chunk()
+
+if __name__ == '__main__':
+	url = 'http://www.ezfshn.com/tides/usa/california/san%20diego'
+	parsed = parse(url)
+
+	doc = parsed.getroot()
+
+	links = doc.findall('.//a')
+
+	links_sub_list = links[15:20]
+	lnk = links_sub_list[0]
+
+	sample_url = lnk.get('href')
+
+	sample_display_text = lnk.text_content()
+
+	tables = doc.findall('.//table')
+
+	dt = tables[0]
+
+	rows = dt.finall('.//tr')
+
+	headers = _unpack(rows[0], kind = 'th')
+
+	row_vals = _unpack(rows[1], kind = 'td')
+
+	tide_data = parse_options_data(dt)
+
+	print tide_data[:10]
+	
