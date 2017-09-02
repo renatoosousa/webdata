526a45d9a48b43c4f8e6a011d2840da5ffd3d7fb
Renato Sousa <rsb5@cin.ufpe.br>
2017-09-02 09:30:49 -0300

    removing stopwords and using lower

--- classifier/study_codes/nwords.py
+++ classifier/study_codes/nwords.py
@@ -1,18 +1,24 @@
-import operator
 import pandas as pd
-import re
+import operator
+import matplotlib.pyplot as plt
+
+def filter_txt(txt):
+    stopw = (open('stopwords.txt', 'r')).read()
+    for word in stopw.split():
+        txt = txt.replace(" "+word+" "," ")
+    txt = txt.decode('utf-8')
+    
+    splits = [',', '(', ')', '[', ']', '.', '!', '?', ';', ':', '/', '|', '"', '+', '-']
+    for split in splits:
+        txt = txt.replace(split, ' ')
+    return txt
+
+url = 'https://www.zapimoveis.com.br/lancamento/apartamento+venda+boa-viagem+recife+pe+jardim-das-orquideas+moura-dubeux+95m2-124m2/ID-13235/?contato=0'
 
 text = open('text.txt', 'r')
-txt = text.read()
+txt = text.read() + '\n' + url
 
-#eleminate some chars
-txt = txt.replace(",", " ")
-txt = txt.replace("(", " ")
-txt = txt.replace(")", " ")
-txt = txt.replace(".", " ")
-txt = txt.replace(",", " ")
-txt = txt.replace("[", " ")
-txt = txt.replace("]", " ")
+txt = filter_txt(txt.lower())
 
 wordcount = {}
 
@@ -23,7 +29,7 @@ for word in txt.split():
 	else:
 		wordcount[word] += 1
 
-#wordcount = sorted(wordcount.items(), key=operator.itemgetter(1))
+wordcount_sorted = sorted(wordcount.items(), key=operator.itemgetter(1))
 
 #for k in wordcount:
 #    print k
@@ -35,3 +41,7 @@ elemns = [wordcount[key] for key in wordcount]
 df = pd.DataFrame.from_dict(elemns).T
 df.columns = header
 #df = df.pivot(header)
+
+#row = df.iloc[0]
+#row.plot(kind='bar')
+#plt.show()
