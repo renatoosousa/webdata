4b21eb761a4affac8c35e5df437ecd1b107261b2
Renato Sousa <rsb5@cin.ufpe.br>
2017-09-09 10:38:06 -0300

    prototype create dataframe

--- classifier/study_codes/nwords.py
+++ classifier/study_codes/nwords.py
@@ -13,35 +13,78 @@ def filter_txt(txt):
         txt = txt.replace(split, ' ')
     return txt
 
-url = 'https://www.zapimoveis.com.br/lancamento/apartamento+venda+boa-viagem+recife+pe+jardim-das-orquideas+moura-dubeux+95m2-124m2/ID-13235/?contato=0'
+def countWords(txt):
+    wordcount = {}
+    
+    for word in txt.split():
+        if word not in wordcount:
+            wordcount[word] = 1
+        else:
+            wordcount[word] += 1
+    return wordcount
 
-text = open('text.txt', 'r')
-txt = text.read() + '\n' + url
+def all_keys(dictlist):
+    return list(set().union(*dictlist))
 
-txt = filter_txt(txt.lower())
+def getValue(dic, word):
+    count = 0
+    try:
+        count = dic[word]
+    except:
+        count = 0
+    return count
 
-wordcount = {}
+def dict2row(dic, header):
+    return [(getValue(dic, word)) for word in header]
 
-#text.read().split()
-for word in txt.split():
-	if word not in wordcount:
-		wordcount[word] = 1
-	else:
-		wordcount[word] += 1
 
-wordcount_sorted = sorted(wordcount.items(), key=operator.itemgetter(1))
+url1 = 'https://www.zapimoveis.com.br/lancamento/apartamento+venda+boa-viagem+recife+pe+jardim-das-orquideas+moura-dubeux+95m2-124m2/ID-13235/?contato=0'
+url2 = 'https://www.zapimoveis.com.br/lancamento/apartamento+venda+boa-viagem+recife+pe+jardim-das-acacias+moura-dubeux+185m2/ID-13234/?contato=0'
+url3 = 'https://revista.zapimoveis.com.br/?utm_source=zapimoveis&utm_medium=link-header&utm_campaign=btn-zapemcasa&_ga=2.41900458.9832649.1504960224-2135475700.1504045699'
+
+text = open('text1.txt', 'r')
+txt1 = text.read() + '\n' + url1
+text.close()
+
+text = open('text2.txt', 'r')
+txt2 = text.read() + '\n' + url2
+text.close()
+
+text = open('text3.txt', 'r')
+txt3 = text.read() + '\n' + url3
+text.close()
+
+txt1 = filter_txt(txt1.lower())
+txt2 = filter_txt(txt2.lower())
+txt3 = filter_txt(txt3.lower())
+dict1 = countWords(txt1)
+dict2 = countWords(txt2)
+dict3 = countWords(txt3)
+
+alldict = [dict1, dict2, dict3]
+allkey = all_keys(alldict)
+
+row1 = dict2row(dict1, allkey)
+row2 = dict2row(dict2, allkey)
+row3 = dict2row(dict3, allkey)
+
+df = pd.DataFrame(columns = allkey)
+df.loc[0] = row1
+df.loc[1] = row2
+df.loc[2] = row3
+#wordcount_sorted = sorted(wordcount.items(), key=operator.itemgetter(1))
 
 #for k in wordcount:
 #    print k
 
-header = [x for x in wordcount]
-elemns = [wordcount[key] for key in wordcount]
-# print(type(wordcount[0]))
-
-df = pd.DataFrame.from_dict(elemns).T
-df.columns = header
-#df = df.pivot(header)
-
-#row = df.iloc[0]
-#row.plot(kind='bar')
-#plt.show()
+#header = [x for x in wordcount]
+#elemns = [wordcount[key] for key in wordcount]
+## print(type(wordcount[0]))
+#
+#df = pd.DataFrame.from_dict(elemns).T
+#df.columns = header
+##df = df.pivot(header)
+#
+##row = df.iloc[0]
+##row.plot(kind='bar')
+##plt.show()
