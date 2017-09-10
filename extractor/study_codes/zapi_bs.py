1233b19eac6da49d6d410b9ff6f8fdd3c1902e8f
vbas <vbas@vbas.home>
2017-09-10 17:20:13 -0300

    added dict with infos

--- extractor/study_codes/zapi_bs.py
+++ extractor/study_codes/zapi_bs.py
@@ -7,13 +7,14 @@ from bs4 import BeautifulSoup
 import re
 
 
-
 agent = { 'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0'}
 
 class Zapi_crawler:
 
 	def __init__(self, start_url):
 		self.start_url = start_url
+		self.data = {}
+		self.title = ""
 
 	def crawl(self):
 		self.get_data(self.start_url)
@@ -26,38 +27,49 @@ class Zapi_crawler:
 		annoucer = soup.find("input", {"id": "hdnNomeAnunciante"})
 		annoucer = annoucer["value"]
 		print annoucer
+		self.title = annoucer
 
-		if annoucer == "Moura Dubeux" :
+		if annoucer == "Moura Dubeux":
+			self.extract_data_mb(soup)
+		elif annoucer == "MRV Engenharia S/A":
 			self.extract_data_mb(soup)
 		else:
 			self.extract_data_normal(soup)
-		
 
 	def extract_data_normal(self, soup):
 		price = soup.find("span",  {"class": "value-ficha"})
-		print price.text
+		self.data[ price(text=True)[1] ] = price(text=True)[2]
 
-		city = soup.find("span", {"class": "logradouro"})
-		print city.text
+		prop = soup.find("h1", {"class": "pull-left"})
+		prop = prop.findChildren()[0](text=True)
 
-		#address = soup.find("h1", {"class": "pull-left"})
-		#for item in address:
-			#print address
-			#print "..."
+		self.title = self.title + " - " + str(prop)
+		print self.title
+
+
+		address = soup.find("span", {"class": "logradouro"})
+		address = address.text.split(',')
+		self.data["Bairro"] = address[0]
+		address = address[1].split('-')
+		self.data["Cidade"] = address[0]
+		self.data["Estado"] = address[1]
+
 
 		uls = soup.find_all("ul", {"class": "unstyled"})
 		for ul in uls:
 			for li in ul.find_all('li'):
+				spans = li.find_all("span", {"class": "text-info"})
 				if li.find("span", {"class": "text-info"}):
-					print li.text
+					self.data[ li(text=True)[1] ] = li(text=True)[0]
 
-		return
+		for key in self.data:
+			print key + ": " + self.data[key]
 
 	def extract_data_mb(self, soup):
 		print "todo"
 
 
 
-url = 'https://www.zapimoveis.com.br/oferta/venda+apartamento+3-quartos+boa-viagem+recife+pe+160m2+RS980000/ID-14533109/?oti=1'
+url = 'https://www.zapimoveis.com.br/lancamento/apartamento+venda+boa-viagem+recife+pe+jardim-das-orquideas+moura-dubeux+95m2-124m2/ID-13235/?contato=0&oti=4'
 ri = Zapi_crawler(url)
 ri.crawl()
