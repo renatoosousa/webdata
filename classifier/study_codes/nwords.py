0304544aed8f2b8eef041728fb89538c9c2ff107
vbas <vbas@cin.ufpe.br>
2017-09-11 18:43:38 -0300

    init dataset creation

--- classifier/study_codes/nwords.py
+++ classifier/study_codes/nwords.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import operator
 import matplotlib.pyplot as plt
+import sys
+sys.path.append('../../extractor/study_codes/')
+
+from zapi_bs import Zapi_crawler
 
 def filter_txt(txt):
     stopw = (open('stopwords.txt', 'r')).read()
@@ -42,7 +46,7 @@ url1 = 'https://www.zapimoveis.com.br/lancamento/apartamento+venda+boa-viagem+re
 url2 = 'https://www.zapimoveis.com.br/lancamento/apartamento+venda+boa-viagem+recife+pe+jardim-das-acacias+moura-dubeux+185m2/ID-13234/?contato=0'
 url3 = 'https://revista.zapimoveis.com.br/?utm_source=zapimoveis&utm_medium=link-header&utm_campaign=btn-zapemcasa&_ga=2.41900458.9832649.1504960224-2135475700.1504045699'
 
-text = open('text1.txt', 'r')
+'''text = open('text1.txt', 'r')
 txt1 = text.read() + '\n' + url1
 text.close()
 
@@ -52,16 +56,22 @@ text.close()
 
 text = open('text3.txt', 'r')
 txt3 = text.read() + '\n' + url3
-text.close()
+text.close()'''
+
+
+wrapper = Zapi_crawler(url1)
+txt1 = wrapper.get_rawHtml()
+print type(txt1)
 
 txt1 = filter_txt(txt1.lower())
-txt2 = filter_txt(txt2.lower())
-txt3 = filter_txt(txt3.lower())
+# txt2 = filter_txt(txt2.lower())
+# txt3 = filter_txt(txt3.lower())
 dict1 = countWords(txt1)
-dict2 = countWords(txt2)
-dict3 = countWords(txt3)
+print dict1['quartos']
+# dict2 = countWords(txt2)
+# dict3 = countWords(txt3)
 
-alldict = [dict1, dict2, dict3]
+'''alldict = [dict1, dict2, dict3]
 allkey = all_keys(alldict)
 
 row1 = dict2row(dict1, allkey)
@@ -71,7 +81,7 @@ row3 = dict2row(dict3, allkey)
 df = pd.DataFrame(columns = allkey)
 df.loc[0] = row1
 df.loc[1] = row2
-df.loc[2] = row3
+df.loc[2] = row3'''
 #wordcount_sorted = sorted(wordcount.items(), key=operator.itemgetter(1))
 
 #for k in wordcount:
