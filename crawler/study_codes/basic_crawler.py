c942d68fbd22b709baae1fb7c1ab75808d4d73f7
cso <cso@cin.ufpe.br>
2017-09-11 23:45:06 -0300

    Basic link searcher

---
+++ crawler/study_codes/basic_crawler.py
@@ -0,0 +1,24 @@
+# from bs4 import BeautifulSoup
+# import requests
+# import sys
+
+# def buscar_links(url):
+# 	if 'http' not in url:
+# 		url = 'http://'+url
+
+# 	lista = []
+# 	i = 0
+# 	r = requests.get(url)
+# 	soup = BeautifulSoup(r.text)
+# 	for a in soup.findAll('a',href = True):
+# 		link = a['href']
+# 		i += 1
+# 		lista.append(link)
+# 		links = str(link).strip('[]').replace("u","").replace("'","")
+# 		print str(i)+") "+str(link)
+# 	print "\n>> Total = %d \n" % i
+# 	sys.exit()
+
+# url = raw_input("url: ")
+# buscar_links(url)
+
