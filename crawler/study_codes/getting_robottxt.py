bc23d7ca513052810e9f8b16b7d40f3d8ae02f0b
cso <cso@cin.ufpe.br>
2017-09-20 00:40:08 -0300

    Function to get robot.txt

---
+++ crawler/study_codes/getting_robottxt.py
@@ -0,0 +1,18 @@
+import urllib2 
+
+DOWNLOAD_URL = 'http://zapimoveis.com.br/robots.txt'
+
+hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'}
+req = urllib2.Request(DOWNLOAD_URL, headers=hdr)
+
+result = urllib2.urlopen(req)
+result_data_set = {"Disallowed":[], "Allowed":[]}
+
+for line in result:
+    if line.startswith('Allow'):    # this is for allowed url
+        result_data_set["Allowed"].append(line.split(': ')[1].split(' ')[0])    # to neglect the comments or other junk info
+    elif line.startswith('Disallow'):    # this is for disallowed url
+        result_data_set["Disallowed"].append(line.split(': ')[1].split(' ')[0])    # to neglect the comments or other junk info
+
+print (result_data_set)
+# print result_data_set["Disallowed"][0]
