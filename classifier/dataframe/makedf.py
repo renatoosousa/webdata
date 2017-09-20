ca9811309b87cc92fea175a176f00fd7caa78b44
Renato Sousa <rsb5@cin.ufpe.br>
2017-09-19 21:40:52 -0300

    create database generator

---
+++ classifier/dataframe/makedf.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 19 20:54:21 2017
+
+@author: renato
+"""
+import sys
+sys.path.append('../study_codes/')
+import nwords as nw
+
+dataframe_path = ''
+
+nw.makedb('db1')
--- classifier/study_codes/nwords.py
+++ classifier/study_codes/nwords.py
@@ -1,6 +1,4 @@
 import pandas as pd
-import operator
-import matplotlib.pyplot as plt
 import sys
 sys.path.append('../../extractor/study_codes/')
 
@@ -43,85 +41,31 @@ def getValue(dic, word):
 def dict2row(dic, header):
     return [(getValue(dic, word)) for word in header]
 
-'''
-url1 = 'https://www.zapimoveis.com.br/lancamento/apartamento+venda+boa-viagem+recife+pe+jardim-das-orquideas+moura-dubeux+95m2-124m2/ID-13235/?contato=0'
-url2 = 'https://www.zapimoveis.com.br/lancamento/apartamento+venda+boa-viagem+recife+pe+jardim-das-acacias+moura-dubeux+185m2/ID-13234/?contato=0'
-url3 = 'https://revista.zapimoveis.com.br/?utm_source=zapimoveis&utm_medium=link-header&utm_campaign=btn-zapemcasa&_ga=2.41900458.9832649.1504960224-2135475700.1504045699'
-
-text = open('text1.txt', 'r')
-txt1 = text.read() + '\n' + url1
-text.close()
-
-text = open('text2.txt', 'r')
-txt2 = text.read() + '\n' + url2
-text.close()
-
-text = open('text3.txt', 'r')
-txt3 = text.read() + '\n' + url3
-text.close()'''
-
-# dataframe with urls and labels
-urls = pd.read_csv('../dataframe/urls2.csv')
-
-wrapper = Zapi_crawler('')
-alldict = []
-
-for index, webpage in urls.iterrows():
-    wrapper.start_url = webpage['site']
-    try:
-        txt = filter_txt(wrapper.get_rawHtml().lower())
-        alldict.append(countWords(txt))
-    except:
-        print("error")
-
-allkeys = all_keys(alldict)
-header = all_keys(alldict)
-header.append('label')
-df = pd.DataFrame(columns = header)
-
-for idx, i_dict in enumerate(alldict):
-    row = dict2row(i_dict, allkeys)
-    row.append(urls.loc[idx]['label'])
-    df.loc[idx] = row
-        
-df.to_csv('database.csv', sep='\t', encoding = 'utf-8')
-#wrapper = Zapi_crawler(url1)
-#txt1 = wrapper.get_rawHtml()
-#print type(txt1)
-
-#txt1 = filter_txt(txt1.lower())
-# txt2 = filter_txt(txt2.lower())
-# txt3 = filter_txt(txt3.lower())
-#dict1 = countWords(txt1)
-
-#print dict1['quartos']
-# dict2 = countWords(txt2)
-# dict3 = countWords(txt3)
-
-'''alldict = [dict1, dict2, dict3]
-allkey = all_keys(alldict)
-
-row1 = dict2row(dict1, allkey)
-row2 = dict2row(dict2, allkey)
-row3 = dict2row(dict3, allkey)
-
-df = pd.DataFrame(columns = allkey)
-df.loc[0] = row1
-df.loc[1] = row2
-df.loc[2] = row3'''
-#wordcount_sorted = sorted(wordcount.items(), key=operator.itemgetter(1))
-
-#for k in wordcount:
-#    print k
-
-#header = [x for x in wordcount]
-#elemns = [wordcount[key] for key in wordcount]
-## print(type(wordcount[0]))
-#
-#df = pd.DataFrame.from_dict(elemns).T
-#df.columns = header
-##df = df.pivot(header)
-#
-##row = df.iloc[0]
-##row.plot(kind='bar')
-##plt.show()
+def makedb(df_name):
+    # dataframe with urls and labels
+    urls = pd.read_csv('../dataframe/urls2.csv')
+    
+    wrapper = Zapi_crawler('')
+    alldict = []
+    
+    for index, webpage in urls.iterrows():
+        wrapper.start_url = webpage['site']
+        try:
+            txt = filter_txt(wrapper.get_rawHtml().lower())
+            alldict.append(countWords(txt))
+        except:
+            print("\n\n" + "error: "+ wrapper.start_url + "\n\n")
+    
+    allkeys = all_keys(alldict)
+    header = all_keys(alldict)
+    header.append('label')
+    df = pd.DataFrame(columns = header)
+    
+    for idx, i_dict in enumerate(alldict):
+        row = dict2row(i_dict, allkeys)
+        row.append(urls.loc[idx]['label'])
+        df.loc[idx] = row
+    
+    df_name = df_name + '.csv'        
+    df.to_csv(df_name, sep='\t', encoding = 'utf-8')
+    print('Save '+ df_name)
