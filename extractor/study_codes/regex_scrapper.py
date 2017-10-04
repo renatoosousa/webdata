ebbea28867353530ad10b359494e1e5baf1f5bdd
vbas <vbas@cin.ufpe.br>
2017-10-04 15:45:10 -0300

    adding urls

--- extractor/study_codes/regex_scrapper.py
+++ extractor/study_codes/regex_scrapper.py
@@ -24,9 +24,9 @@ class Regex_scrapper:
         html = self.get_rawHtml()
 
         try:
-            quartos = re.search("\d+\squartos?", html)
+            quartos = re.search("(\d+\s[Qq]uartos?) | ([Qq]uartos?(\s:?\s)\d+)", html)
             valor = re.search("R\$\s\d+[,.]\d+", html)
-            vagas = re.search("\d+\svagas?",html)
+            vagas = re.search("[vV]agas? | (\d+\s?[vV]agas?)",html)
             area = re.search("\d+\sarea", html)
             tipo = re.search("locacao|aluguel", html)
 
@@ -51,10 +51,21 @@ class Regex_scrapper:
         for js in soup(["script", "style"]):
             js.extract()
 
-        return soup.get_text().encode("utf-8")
+        return soup.body.get_text().encode("utf-8")
+urls = [
+        'https://www.zapimoveis.com.br/lancamento/apartamento+venda+socorro+jaboatao-dos-guararapes+pe+reserva-villa-natal--goiabeiras+mrv-engenharia-s-a+49m2/ID-9704/?oti=1',
+        'https://www.expoimovel.com/imovel/apartamentos-comprar-vender-imbui-salvador-bahia/389449/pt/BR',
+        'http://pe.olx.com.br/grande-recife/imoveis/excelente-locacao-engenho-do-meio-397980090',
+        'http://www.directimoveis.com.br/imovel-V0373/morada-da-peninsula',
+        'http://imovelavenda.com.br/Fiateci_Apto_2_Dorm_Sao_Geraldo_Porto_Alegre_10066_RS__YWQ10',
+        'http://www.imovelweb.com.br/propriedades/apartamento-residencial-para-locacao-boa-viagem-2932218008.html',
+        'http://diariodepernambuco.lugarcerto.com.br/imovel/casa-em-condominio-2-quartos-aldeia-camaragibe-54m2-venda-rs230000-id-329299726',
+        'http://www.redeimoveispe.com.br/imovel-detalhes.aspx?refRede=AP0193-CZA',
+        'http://www.teuimovel.com/index_detalheimovel_id_1783_desc_sao-paulo-sp-paraIso',
+        'https://www.vivareal.com.br/imovel/apartamento-3-quartos-butanta-zona-oeste-sao-paulo-com-garagem-70m2-venda-RS369000-id-84167740/?__vt=map:b',
+]
 
-url = 'https://www.zapimoveis.com.br/lancamento/apartamento+venda+socorro+jaboatao-dos-guararapes+pe+reserva-villa-natal--goiabeiras+mrv-engenharia-s-a+49m2/ID-9704/?oti=1'
-url2 = 'https://www.expoimovel.com/imovel/apartamentos-comprar-vender-imbui-salvador-bahia/389449/pt/BR'
-url3 = 'http://pe.olx.com.br/grande-recife/imoveis/excelente-locacao-engenho-do-meio-397980090'
-ri = Regex_scrapper(url3)
+
+print urls
+ri = Regex_scrapper(urls[0])
 ri.crawl()
