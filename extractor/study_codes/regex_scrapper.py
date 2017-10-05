e19c18556c8740eee13a10e53246446ea3ee35b3
vbas <vbas@cin.ufpe.br>
2017-10-05 01:42:57 -0300

    trying preprocessing

--- extractor/study_codes/regex_scrapper.py
+++ extractor/study_codes/regex_scrapper.py
@@ -8,6 +8,7 @@ import re
 
 
 
+file = open("/Users/vbas/Documents/UFPE/RI/htmls.html", "w")
 
 agent = { 'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0',
           'Accept-Language': 'pt-BR'}
@@ -19,12 +20,16 @@ class Regex_scrapper:
         self.data = {}
         self.title = ""
 
-
     def crawl(self):
-        html = self.get_rawHtml()
+        html = self.pre_processing()
 
+        print html
+        file.write(html)
+        file.write("\n------------------------------------------------------------------------ FIM DO HTML --------------------------------------------------------------------------------------")
+
+        quarto_regex = "(\d+\s?\n[qQ]uartos?) | ( [Qq]uartos?:?\n+\d+)"
         try:
-            quartos = re.search("(\d+\s[Qq]uartos?) | ([Qq]uartos?(\s:?\s)\d+)", html)
+            quartos = re.search(quarto_regex, html)
             valor = re.search("R\$\s\d+[,.]\d+", html)
             vagas = re.search("[vV]agas? | (\d+\s?[vV]agas?)",html)
             area = re.search("\d+\sarea", html)
@@ -35,7 +40,7 @@ class Regex_scrapper:
 
         try:
             print "valor: " + valor.group()
-            print "quartos: " + quartos.group()
+            print "quartos: " + quartos.group().replace('\n', '')
             print "vagas: " + vagas.group()
             print "area: " + area.group() 
             print "tipo: "+ tipo.group()
@@ -44,14 +49,31 @@ class Regex_scrapper:
 
         return
 
-    def get_rawHtml(self):
+    def pre_processing(self):
         start_page = requests.get(self.start_url, headers = agent)
         soup = BeautifulSoup(start_page.content, "html.parser")
 
         for js in soup(["script", "style"]):
             js.extract()
 
-        return soup.body.get_text().encode("utf-8")
+
+        '''for span in soup("span"):
+             span.append(soup.new_tag('br'))
+
+        for li in soup("li"):
+            li.append(soup.new_tag('br'))
+
+        for div in soup("li"):
+            div.append(soup.new_tag('br'))
+
+        for i in soup("i"):
+            i.append(soup.new_tag('br'))
+
+        for p in soup("p"):
+            p.append(soup.new_tag('br'))'''
+        
+
+        return soup.get_text().encode("utf-8")
 urls = [
         'https://www.zapimoveis.com.br/lancamento/apartamento+venda+socorro+jaboatao-dos-guararapes+pe+reserva-villa-natal--goiabeiras+mrv-engenharia-s-a+49m2/ID-9704/?oti=1',
         'https://www.expoimovel.com/imovel/apartamentos-comprar-vender-imbui-salvador-bahia/389449/pt/BR',
@@ -61,11 +83,12 @@ urls = [
         'http://www.imovelweb.com.br/propriedades/apartamento-residencial-para-locacao-boa-viagem-2932218008.html',
         'http://diariodepernambuco.lugarcerto.com.br/imovel/casa-em-condominio-2-quartos-aldeia-camaragibe-54m2-venda-rs230000-id-329299726',
         'http://www.redeimoveispe.com.br/imovel-detalhes.aspx?refRede=AP0193-CZA',
-        'http://www.teuimovel.com/index_detalheimovel_id_1783_desc_sao-paulo-sp-paraIso',
+        'http://www.teuimovel.com/index_detalheimovel_id_2116_desc_sao-paulo-sp-vila-silvia',
         'https://www.vivareal.com.br/imovel/apartamento-3-quartos-butanta-zona-oeste-sao-paulo-com-garagem-70m2-venda-RS369000-id-84167740/?__vt=map:b',
 ]
 
+for url in urls:
+    ri = Regex_scrapper(url)
+    ri.crawl()
 
-print urls
-ri = Regex_scrapper(urls[0])
-ri.crawl()
+file.close()
