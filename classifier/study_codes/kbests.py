ce693c93b1dfd6a770b1dba385d8ed401ddd5c5f
Renato Sousa <rsb5@cin.ufpe.br>
2017-10-07 01:01:05 -0300

    fix problemns with df7 and df8

--- classifier/study_codes/kbests.py
+++ classifier/study_codes/kbests.py
@@ -8,6 +8,7 @@ Created on Thu Oct  5 21:13:42 2017
 
 import pandas as pd
 import numpy as np
+import operator
 from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import chi2
 
@@ -17,28 +18,58 @@ def row2col(a):
         aux[i,0] = a[i]
     return aux
 
+def select_kbest(dfx, dfy):
+    columns = dfx.columns
+    selector = SelectKBest(chi2, k=1000)
+    selector.fit_transform(dfx,dfy)
+    labels = [columns[x] for x in selector.get_support(indices=True) if x]
+    return pd.DataFrame(selector.fit_transform(dfx, dfy), columns=labels)
+
+
 df6 = pd.read_csv('../dataframe/db6.csv', sep = '\t')
 
-#a = df6.values
-#X = a[:,:a.shape[1]-1]
-#y = a[:,a.shape[1]-1]
-#print(X.shape)
-#X_new = SelectKBest(chi2, k=1000).fit_transform(X, y)
-#print(X_new.shape)
-#new_df = np.append(X_new,row2col(y), axis=1)
-#df7 = pd.DataFrame(new_df)
+'''
+make df7 dataframe
+'''
+#headers = list(df6.columns.values)
+#headers.remove('label')
+#headers.remove('Unnamed: 0')
+#
+#df6x = df6[headers]
+#df6y = df6['label']
+#
+#df7 = select_kbest(df6x,df6y)
+#df7['label'] = df6y
 #df7.to_csv('../dataframe/db7.csv', sep='\t', encoding = 'utf-8')
 
 
-df6 = df6.drop('Unnamed: 0', axis=1)
-labels = row2col(df6['label'].values)
-df6 = df6.drop('label', axis=1)
+'''
+make df8 dataframe
+'''
+headers = list(df6.columns.values)
+headers.remove('label')
+headers.remove('Unnamed: 0')
 
-a = df6.values
-b = np.sum(a, axis=0)
-idx = b.argsort()
-r = np.take(a,idx,axis=1)
-s = r[:,r.shape[1]-1001:r.shape[1]-1]
-s = np.append(s,labels, axis=1)
-df8 = pd.DataFrame(s)
+freq = dict((el,int(df6[el].sum())) for el in headers)
+sorted_freq = sorted(freq.items(), key=operator.itemgetter(1))
+k_freq = sorted_freq[-1000:]
+
+new_headers = [i[0] for i in k_freq]
+
+df8 = df6[new_headers]
+df8['label'] = df6['label']
 df8.to_csv('../dataframe/db8.csv', sep='\t', encoding = 'utf-8')
+
+
+#df6 = df6.drop('Unnamed: 0', axis=1)
+#labels = row2col(df6['label'].values)
+#df6 = df6.drop('label', axis=1)
+#
+#a = df6.values
+#b = np.sum(a, axis=0)
+#idx = b.argsort()
+#r = np.take(a,idx,axis=1)
+#s = r[:,r.shape[1]-1001:r.shape[1]-1]
+#s = np.append(s,labels, axis=1)
+#df8 = pd.DataFrame(s)
+#df8.to_csv('../dataframe/db8.csv', sep='\t', encoding = 'utf-8')
