47d467797a8a6b1bdc7e677d6fb80a57e9fea48d
Renato Sousa <rsb5@cin.ufpe.br>
2017-12-01 20:15:12 -0300

    first commit second part project

---
+++ ir/query_processing/query.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  1 19:56:18 2017
+
+@author: renato
+"""
+import random
+
+qts = random.sample(range(20), 10)
+area = random.sample(range(20), 10)
+local = random.sample(range(20), 10)
+qts.sort()
+area.sort()
+local.sort()
+# Reading Postings
