57b37d406947d264e00609009da6967d78050799
Renato Sousa <rsb5@cin.ufpe.br>
2017-12-06 15:44:09 -0300

    parse fucntion

--- ir/query_processing/query.py
+++ ir/query_processing/query.py
@@ -48,20 +48,37 @@ def ranking(dic, query):
         result.setdefault(key,vectorSpace(dic[key],query))
     return sorted(result.items(), key=lambda x:x[1], reverse=True)    
 
-def term_frequency(term, tokenized_document):
-    return tokenized_document.count(term)
-
-def inverse_document_frequencies(tokenized_documents):
-    idf_values = {}
-    all_tokens_set = set([item for sublist in tokenized_documents for item in sublist])
-    for tkn in all_tokens_set:
-        contains_token = map(lambda doc: tkn in doc, tokenized_documents)
-        idf_values[tkn] = 1 + math.log(len(tokenized_documents)/(sum(contains_token)))
-    return idf_values
-
-def ranking_tfidf(vec,qry,post):
+def parser(request):
+    # quartos 1 2 3 4 5 6
+    rangeqts = [1,2,3,4,5,6]
+    # banheiro 1 2 3 4 5 6
+    rangebanheiro = [1,2,3,4,5,6]
+    # vaga 1 2 3 4 5
+    rangevaga = [1,2,3,4,5]
+    # valor
+    # cidade
+    qry = []
+    # Quartos
+    for value in rangeqts:
+        if(request.get('quartos') == value):
+            qry.append(1)
+        else:
+            qry.append(0)
     
-    return vec
+    # Banheiros
+    for value in rangebanheiro:
+        if(request.get('banheiros') == value):
+            qry.append(1)
+        else:
+            qry.append(0)
+            
+    # vagas
+    for value in rangevaga:
+        if(request.get('vaga') == value):
+            qry.append(1)
+        else:
+            qry.append(0)
+    return qry
 
 def spearman(r1,r2):
     _sum = 0
@@ -90,28 +107,44 @@ qts1 = random.sample(range(20), 6)
 qts2 = random.sample(range(20), 8)
 qts3 = random.sample(range(20), 14)
 qts4 = random.sample(range(20), 12)
+qts5 = random.sample(range(20), 14)
+qts6 = random.sample(range(20), 12)
 area1 = random.sample(range(20), 5)
 area2 = random.sample(range(20), 17)
 area3 = random.sample(range(20), 3)
 area4 = random.sample(range(20), 5)
+area5 = random.sample(range(20), 3)
+area6 = random.sample(range(20), 5)
 local1 = random.sample(range(20), 15)
 local2 = random.sample(range(20), 15)
 local3 = random.sample(range(20), 10)
+local4 = random.sample(range(20), 15)
+local5 = random.sample(range(20), 10)
 
 qts1.sort()
 qts2.sort()
 qts3.sort()
 qts4.sort()
+qts5.sort()
+qts6.sort()
 area1.sort()
 area2.sort()
 area3.sort()
 area4.sort()
+area5.sort()
+area6.sort()
 local1.sort()
 local2.sort()
+local3.sort()
+local4.sort()
+local5.sort()
 
-allPostings = [qts1,qts2,qts3,qts4,area1,area2,area3,area4,local1,local2]
 
-query = [1,0,1,0,1,0,0,0,1,1]
+request = {"cidade": "Recife", "quartos": 4, "banheiros": 2, "valor": 1000, "vaga": 3}
+
+allPostings = [qts1,qts2,qts3,qts4,qts5,qts6,area1,area2,area3,area4,area5,area6,local1,local2,local3,local4,local5]
+
+query = parser(request)
 
 # Reading Postings
 vectorized = termAtTime(allPostings)
@@ -126,3 +159,4 @@ rank_tfidf = rank
 
 spm = spearman([int(i[0]) for i in rank], [int(i[0]) for i in rank_tfidf])
 kt = kendalTau([int(i[0]) for i in rank], [int(i[0]) for i in rank_tfidf])
+#print(parser(request))
