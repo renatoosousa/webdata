452174395d4729cd70f14a0dcd782456a412495a
vbas <vbas@cin.ufpe.br>
2017-12-06 01:29:05 -0300

    add banheiros processing

--- extractor/study_codes/reverse_index.py
+++ extractor/study_codes/reverse_index.py
@@ -4,6 +4,7 @@ from collections import defaultdict
 regex_value = "[Vv][aA][lL][oO][Rr](.+)?:\s?R\$\s?\d+[,.]\d+([,.]\d+)?"
 regex_quartos = ["[Qq][uU][aA][rR][tT].+:\s\d", "[Dd][Oo][Rr][Mm].+:\s\d"]
 regex_cidade = ["[cC][iI][dD][aA][dD].+", "[mM][uU][nN][iI][cC].+", "[eE][nN][dD][eE][rR][eE].+"]
+regex_banheiro = ["[bB][aA][nN][hH][eE].+", "[sS][uU].+:\s\d"]
 
 def get_quartos(data):
 
@@ -31,7 +32,7 @@ def get_cidade(data):
                 cidade = cidade[-1].split('-')
                 cidade = cidade[0]
                 #print cidade
-                return cidade
+                return cidade.lower()
 
 
     try:
@@ -39,11 +40,25 @@ def get_cidade(data):
         cidade = cidade[1]
     except:
         cidade = -1
+        return cidade
     #print cidade
-    return cidade
+    return cidade.lower()
+
+def get_banheiros(data):
+    banheiro = re.findall(regex_banheiro[0], data)
+    if not banheiro:
+        banheiro = re.findall(regex_banheiro[1], data)
+
+    if banheiro:
+        banheiro = banheiro[0].split(' ')
+        banheiro = int(banheiro[1])
+        return banheiro
+    else:
+        return -1
 
 doc = "results/docs/doc_"
 dist_quartos = {}
+dist_banheiros = {}
 for i in range(255,355):
     doc_name = doc + str(i) + ".txt"
     with open(doc_name) as f:
@@ -55,14 +70,26 @@ for i in range(255,355):
             dist_quartos[quartos] = 1
         else:
             dist_quartos[quartos] += 1
+        print "quartos: ", quartos
 
         #processing cidade
         cidade = get_cidade(data)
-        print cidade
-        print doc_name
-        z = raw_input()
+        print "cidade: ", cidade
+        print "doc id: ", i
+
+
+        #processing banheiro
+        banheiros = get_banheiros(data)
+        if not dist_banheiros.get(banheiros):
+            dist_banheiros[banheiros] = 1
+        else:
+            dist_banheiros[banheiros] += 1
+        print "banheiros: ", banheiros
+        #z = raw_input()
 
 print "distribuicao quartos: ", dist_quartos
+print "distribuicao banheiros: ", dist_banheiros
+
 # sum = 0
 # for item in dist_quartos:
 #     sum += dist_quartos[item]
