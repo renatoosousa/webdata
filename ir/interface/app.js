8ab15ca0d77fab0d673405d4c9f21154614880ed
cso <cso@cin.ufpe.br>
2017-12-07 11:49:00 -0300

    Inferce feita

--- ir/interface/app.js
+++ ir/interface/app.js
@@ -2,6 +2,9 @@ var ed = require('edit-distance');
 var fs = require('fs');
 var path = require('path');
 filePath = path.join(__dirname, 'dic_city.txt');
+filePath1 = path.join(__dirname, 'dic_city.txt');
+filePath2 = path.join(__dirname, 'stop_words.txt');
+filePath3 = path.join(__dirname, 'comum_word.txt');
 
 /*Importar configuracoes do servidor*/
 var app = require('./config/server');
@@ -15,6 +18,14 @@ function uintToString(uintArray) {
     var encodedString = String.fromCharCode.apply(null, uintArray),
         decodedString = decodeURIComponent(escape(encodedString));
     return decodedString;
+}
+
+function getED(A,B){
+    var insert, remove, update;
+    insert = remove = function(node) { return 1; };
+    update = function(stringA, stringB) { return stringA !== stringB ? 1 : 0; };
+    var lev = ed.levenshtein(A.toLowerCase(), B.toLowerCase(), insert, remove, update);
+    return lev.distance;
 }
 
 var io = require('socket.io').listen(server);
@@ -50,10 +61,63 @@ io.on('connection', function(socket){
 		});
 
 		socket.on('buscaUnica', function(data){
-	    	socket.emit(
-				'erroBuscaUnica', 
-				{mensagem : ["primeiro","segundo","terceiro"]}
-			);
+			fs.readFile(filePath1, {encoding: 'utf-8'}, function(err,dic_city){
+			fs.readFile(filePath2, {encoding: 'utf-8'}, function(err,stop_words){
+			fs.readFile(filePath3, {encoding: 'utf-8'}, function(err,comum_word){
+				dic_city = dic_city.split("\n");
+				stop_words = stop_words.split("\n");
+				comum_word = comum_word.split("\n");
+			    
+			    frase = data.mensagem.split(' ');
+			    var calc = 100000;
+
+			    var wordCor = new Array(frase.length);
+				for (var i = 0; i < frase.length; i++) {
+			  		wordCor[i] = [];
+				}
+			    for(var i = 0; i<frase.length; i++){
+			   		var temp = 1000000;
+
+			   		for(var j = 0; j<dic_city.length; j++){
+			    		res = getED(frase[i],dic_city[j]);
+			    		if(res<temp){
+			    			temp = res;
+			    			if(wordCor[i].length>0)wordCor[i].splice(0,1);
+			    			wordCor[i].push(dic_city[j].toLowerCase());
+			    		}
+			    	}
+			    	for(var j = 0; j<stop_words.length; j++){
+			    		res = getED(frase[i],stop_words[j]);
+			    		if(res<temp){
+			    			temp = res;
+			    			wordCor[i].splice(0,1);
+			    			wordCor[i].push(stop_words[j].toLowerCase());
+			    		}
+			    	}
+			    	for(var j = 0; j<comum_word.length; j++){
+			    		res = getED(frase[i],comum_word[j]);
+			    		if(res<temp){
+			    			temp = res;
+			    			wordCor[i].splice(0,1);
+			    			wordCor[i].push(comum_word[j].toLowerCase());
+			    		}
+			    	}
+			    }
+
+			    var recomen = [];
+			    for (var i = 0; i <frase.length ; i++) {
+			    	recomen.push(wordCor[i]);
+			    }
+			    recomen = recomen.join(' ');
+			    console.log(recomen)
+		    	socket.emit(
+					'erroBuscaUnica', 
+					{mensagem : recomen}
+				);
+			});
+			});
+			});	    	
+
 		});
 
 		socket.on('buscaByCity', function(data){
